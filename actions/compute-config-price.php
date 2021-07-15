285dcc6a643df1c93fe4b28a5324f8d56388a29b
Moonpearl <romain.tello@gmail.com>
2021-07-15 10:12:25 +0200

    Refactor config price computing as action file

---
+++ actions/compute-config-price.php
@@ -0,0 +1,52 @@
+<?php
+
+include '../data/config.php';
+
+// Si les query parameters ne sont pas vides, c'est donc que l'utilisateur vient de valider le formulaire
+if (!empty($_GET)) {
+    // Vérifie que toutes les clés indispensables dans le formulaire sont bien présentes dans les query parameters
+    if (isset($_GET['cpu']) &&
+        isset($_GET['ram']) &&
+        isset($_GET['gpu']) &&
+        isset($_GET['os'])
+    ) {
+        // Récupère l'index des différents composants dans les query parameters
+        $cpuIndex = intval($_GET['cpu']);
+        $ramIndex = intval($_GET['ram']);
+        $gpuIndex = intval($_GET['gpu']);
+        $osIndex = intval($_GET['os']);
+    
+        // Ajoute le prix de chaque composant au prix total
+        $totalPrice =
+            $cpus[$cpuIndex]['price'] +
+            $rams[$ramIndex]['price'] +
+            $gpus[$gpuIndex]['price'] +
+            $oss[$osIndex]['price']
+        ;
+        
+        // Si la case "clavier" a été cochée
+        if (isset($_GET['keyboard']) && $_GET['keyboard'] === 'on') {
+            // Ajoute la prix du clavier au prix total
+            $totalPrice += $accessories['keyboard']['price'];
+        }
+
+        // Si la case "souris" a été cochée
+        if (isset($_GET['mouse']) && $_GET['mouse'] === 'on') {
+            // Ajoute la prix du souris au prix total
+            $totalPrice += $accessories['mouse']['price'];
+        }
+
+        // Si la case "écran" a été cochée
+        if (isset($_GET['screen']) && $_GET['screen'] === 'on') {
+            // Ajoute la prix du écran au prix total
+            $totalPrice += $accessories['screen']['price'];
+        }
+
+        // Affiche le prix total dans la page
+        echo $totalPrice;
+    // Sinon, c'est que la requête HTTP a été falsifiée
+    } else {
+        // Affiche un message d'erreur dans la page
+        echo 'Formulaire invalide';
+    }
+}
---
+++ data/config.php
@@ -0,0 +1,88 @@
+<?php
+
+$cpus = [
+    [
+        'name' => 'Acu j3 Dual-Core (9ème génération)',
+        'price' => 125,
+    ],
+    [
+        'name' => 'Acu j5 Quad-Core (9ème génération)', 
+        'price' => 250
+    ],
+    [
+        'name' => 'Acu j7 Octo-Core (9ème génération)', 
+        'price' => 500
+    ],
+];
+
+$rams = [
+    [
+        'name' => '2 x 4 Go π-Rate DDR4',
+        'price' => 40
+    ],
+    [
+        'name' => '1 x 8 Go π-Rate DDR4',
+        'price' => 50,
+    ],
+    [
+        'name' => '2 x 8 Go π-Rate DDR4',
+        'price' => 80,
+    ],
+    [
+        'name' => '1 x 16 Go π-Rate DDR4',
+        'price' => 100,
+    ],
+    [
+        'name' => '2 x 16 Go π-Rate DDR4',
+        'price' => 160,
+    ],
+    [
+        'name' => '1 x 32 Go π-Rate DDR4',
+        'price' => 200,
+    ]
+];
+
+$gpus = [
+    [
+        'name' => 'MSCOPIA VeStrength 1050 GTX',
+        'price' => 300
+    ],
+    [
+        'name' => 'MSCOPIA VeStrength 1650 GTX',
+        'price' => 600
+    ],
+    [
+        'name' => 'MSCOPIA VeStrength 2050 GTX',
+        'price' => 900
+    ]
+];
+
+$oss = [
+    [
+        'name' => 'Pas de système d\'exploitation', 
+        'price' => 0
+    ],
+    [
+        'name' => 'Nanosoft Shutters® 10 - édition familiale', 
+        'price' => 100
+    ],
+    [
+        'name' => 'Nanosoft Shutters® 10 - édition professionnelle', 
+        'price' => 100
+    ]
+];
+
+$accessories = [
+    'keyboard' => [
+        'name' => 'Clavier gamer Rationtech K250',
+        'price' => 100
+    ],
+    'mouse' => [
+        'name' => 'Souris gamer Rationtech K502',
+        'price' => 80
+    ],
+    'screen' => [
+        'name'=>'Ecran MSCOPIA MB2042',
+        'price' => 300
+    ]
+];
